703a737bc80746c47ca529ed840d6300847ff860
unknown <yaradayoub2019@gmail.com>
2025-12-03 20:41:43 +0300

    add timeout

--- helpers/getPage.js
+++ helpers/getPage.js
@@ -3,7 +3,11 @@ import { chromium } from "playwright";
 export async function getPage(url) {
   const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto(url, { waitUntil: "networkidle" });
+  page.setDefaultTimeout(0);
+  page.setDefaultNavigationTimeout(0);
+
+  await page.goto(url, { waitUntil: "domcontentloaded", timeout: 0 });
+  await page.waitForTimeout(2000);
 
   return { browser, page };
 }
